b622122178b201a9d1d576f577a1f5b314fa82d6
sean hacmon <100718234+SeanHacmon@users.noreply.github.com>
2022-06-14 18:14:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadedBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadedBinarySearchTree t = new ThreadedBinarySearchTree();
+            for (int i = 0; i <= 10; i++)
+            {
+                t.add(i);
+            }
+            Thread t1 = new Thread(() => t.remove(6));
+            Thread t2 = new Thread( t.print);
+            Thread t3 = new Thread(() => t.search(14));
+            Thread t4 = new Thread(() => t.search(5));
+            Thread t5 = new Thread(() => t.clear());
+            Thread t6 = new Thread(() => t.print());
+            t1.Start();
+            t1.Join();
+            t2.Start();
+            t2.Join();
+            t3.Start();
+            t3.Join();
+            t4.Start();
+            t4.Join();
+            t6.Start();
+            t6.Join();
+            t5.Start();
+            t5.Join();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThreadedBinarySearchTree.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadedBinarySearchTree
+{
+    class Node
+    {
+        public Node left = null;
+        public Node right = null;
+        public int data = int.MinValue;       
+    }
+
+
+    public class ThreadedBinarySearchTree
+    {
+        // init
+        private ReaderWriterLockSlim nodeLock = new ReaderWriterLockSlim();
+        private Node root{ get; set;}
+
+        // constructor
+        public ThreadedBinarySearchTree(){}
+
+        // adding a node after searching it's not in the tree
+        public void add(int num)
+        {
+            if (!search(num))
+            {
+                try
+                {
+                    // Requesting to enter critical section.
+                    nodeLock.EnterWriteLock();
+                    Node curr = this.root;
+                    if (this.root == null)
+                    {
+                        this.root = new Node();
+                        this.root.data = num;
+                    }
+                    else
+                    {
+                        while (curr != null)
+                        {
+                            if (curr.data < num)
+                            {
+                                if (curr.right != null)
+                                    curr = curr.right;
+                                else
+                                {
+                                    curr.right = new Node();
+                                    curr.right.data = num;
+                                    return;
+                                }
+                            }
+                            else // curr.data > num
+                            {
+                                if (curr.left != null)
+                                    curr = curr.left;
+                                else
+                                {
+                                    curr.left = new Node();
+                                    curr.left.data = num;
+                                    return;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                finally
+                {
+                    // Release the Lock.
+                    nodeLock.ExitWriteLock();
+                }
+            }
+
+        }
+
+
+        public void remove(int num)
+        {
+            if (search(num))
+            {
+                try
+                {
+                    nodeLock.EnterWriteLock();
+                    Node curr = this.root;
+                    this.root = deleteRec(root, num);
+                }
+                finally
+                {
+                    // Release the Lock.
+                    nodeLock.ExitWriteLock();
+                }
+            }
+        }
+
+        public bool search(int num)
+        {
+            // Entering critical section.
+            nodeLock.EnterReadLock();
+            try
+            {
+                Node curr = this.root;
+                while (curr != null)
+                {
+                    if (curr.data == num)
+                        return true;
+                    if (curr.data < num)
+                        curr = curr.left;
+                    else if (curr.data > num)
+                        curr = curr.right;
+                }
+                    return false;
+            }
+            finally
+            {
+                // Release the Lock.
+                nodeLock.ExitReadLock();
+            }
+     
+        }
+
+        public void clear()
+        {
+            while (this.root != null)
+            {
+                remove(this.root.data);
+            }
+        }
+
+        public void print()
+        {
+            nodeLock.EnterReadLock();
+            try
+            {
+                inorderRec(this.root);
+            }
+            finally
+            {
+                nodeLock.ExitReadLock();
+            }
+        }
+
+        // Helper Functions.
+
+        private Node deleteRec(Node root, int key)
+        {
+            /* Base Case: If the tree is empty */
+            if (root == null)
+                return root;
+
+            /* Otherwise, recur down the tree */
+            if (key < root.data)
+                root.left = deleteRec(root.left, key);
+            else if (key > root.data)
+                root.right = deleteRec(root.right, key);
+
+            // if key is same as root's
+            // key, then This is the
+            // node to be deleted
+            else
+            {
+                // node with only one child or no child
+                if (root.left == null)
+                    return root.right;
+                else if (root.right == null)
+                    return root.left;
+
+                // node with two children: Get the inorder
+                // successor (smallest in the right subtree)
+                root.data = minValue(root.right);
+
+                // Delete the inorder successor
+                root.right = deleteRec(root.right, root.data);
+            }
+
+            return root;
+        }
+
+        private int minValue(Node root)
+        {
+            int minv = root.data;
+            while (root.left != null)
+            {
+                minv = root.left.data;
+                root = root.left;
+            }
+            return minv;
+        }
+
+        private void inorderRec(Node root)
+        {
+            if (root != null)
+            {
+                inorderRec(root.left);
+                Console.Write(root.data + " ");
+                inorderRec(root.right);
+            }
+        }
+
+        }
+
+}
